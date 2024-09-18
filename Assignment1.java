f2ff4ab0d19e7d43ec2ae9766bdd52250f666dbe
Ahmad Chishti <119443929+AhmadC14@users.noreply.github.com>
2024-09-18 16:57:41 -0400

    Finished Assignment1.java
    My program prints my first and last initials (AC) in large block letters just as instructed in the markdown file. Then, my code asks the user to input a number in Fahrenheit. My program converts the value to Celsius and saves it.  Then, my program takes input from the user in the form of a 5-character string. It saves the string in reverse without the first and last characters. Them, my program generates and saves a random number between 32 and 16384 (inclusive). Finally, my program combines the results of the 2nd, 3rd, and 4th steps as per the markdown file and prints it to the screen. Then my program closes the scanner and my program is finished.

--- Assignment1.java
+++ Assignment1.java
@@ -1,7 +1,39 @@
-// Import libraries here
+import java.util.Scanner;
+import java.util.Random;
 
 public class Assignment1 {
-  public static void main(String[] args) {
-    // Add your code here
-  }
+    public static void main(String[] args) {
+        System.out.println("      AAA         CCCCCCCCCCCC");
+        System.out.println("     AAAAA        CCCCCCCCCCCC");
+        System.out.println("   AA     AA      CCC");
+        System.out.println("  AAA     AAA     CCC");
+        System.out.println("AAAAAAAAAAAAAAA   CCC");
+        System.out.println("AAAAAAAAAAAAAAA   CCC");
+        System.out.println("AAAA       AAAA   CCC");
+        System.out.println("AAAA       AAAA   CCCCCCCCCCCC");
+        System.out.println("AAAA       AAAA   CCCCCCCCCCCC");
+        System.out.println("");
+        
+        Scanner scan = new Scanner(System.in);
+        System.out.println("Please enter a number in Fahrenheit: ");
+        double fahrenheit = scan.nextDouble();
+        double celsius = (fahrenheit - 32) * 5/9;
+        
+        System.out.println("Please enter a 5-character string: ");
+        String stringInputted = scan.next();
+        if (stringInputted.length() == 5) {
+            String middle = stringInputted.substring(1, 4);
+            String stringBackwards = new StringBuilder(middle).reverse().toString();
+            
+            Random random = new Random();
+            int randomNumber = random.nextInt(16384 - 32 + 1) + 32;
+            
+            System.out.println("Random number generated. Continuing...");
+            System.out.println("Your new string is " + celsius + stringBackwards + randomNumber);
+        } else {
+            System.out.println("Please enter exactly 5 characters!");
+        }
+        
+        scan.close();
+    }
 }
